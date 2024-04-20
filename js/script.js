857b6f6881ed48998a2579614cbd11f5f7a42047
Leonel Alamo <leomovio5@gmail.com>
2024-04-20 13:54:27 -0300

    Finalizando el Responsive

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ function sacarSidebar(){
 
 function mostrarSidebar(){
     const sideBar = document.getElementById('sidebar');
+    const body = document.getElementsByTagName('body');
     sideBar.style.display = 'flex';
+
+    body.style.backgroundColor = 'gray';
+
 
 }
