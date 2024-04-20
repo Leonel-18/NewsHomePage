5d1bc84f0669dfcafaff3d56d5a6258ff4e524b4
Leonel Alamo <leomovio5@gmail.com>
2024-04-20 13:05:34 -0300

    Terminando Responsive Desing

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+function sacarSidebar(){
+    const sideBar = document.getElementById('sidebar');
+
+    sideBar.style.display = 'none';
+}
+
+function mostrarSidebar(){
+    const sideBar = document.getElementById('sidebar');
+    sideBar.style.display = 'flex';
+
+}
